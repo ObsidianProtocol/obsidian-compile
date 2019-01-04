26ea78636cfa547d35c2acf6fa7ed2d069d66e24
Syahrul <pauscrypto@gmail.com>
2019-01-04 20:36:31 +0800

    linux compile

---
+++ linuxcompile.sh
@@ -0,0 +1,16 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb https://apt.llvm.org/xenial/ llvm-toolchain-xenial 6.0 main"
+sudo apt-get update
+sudo apt-get install aptitude -y
+sudo aptitude install -y -o Aptitude::ProblemResolver::SolutionCost='100*canceled-actions,200*removals' build-essential clang-6.0 libstdc++-7-dev git libboost-all-dev python-pip
+sudo pip install cmake
+export CC=clang-6.0
+export CXX=clang++-6.0
+git clone -b Obsidian-Development --single-branch https://github.com/ObsidianProtocol/obsidian.git
+cd turtlecoin
+mkdir build
+cd build
+cmake ..
+make
+
